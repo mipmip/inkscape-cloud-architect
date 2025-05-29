bea32b90cbd999428c37b8c6c7cf68f211aacf80
Pim Snel <post@pimsnel.com>
2025-05-29 12:23:54 +0200

    feat: add initial flake.nix configuration

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Inkscape Cloud Architect";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
